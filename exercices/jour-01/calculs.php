128d3cabe1e1f371c9d693dd5fb8b769f305ef89
Maeva Pereira <maeva.pereira@le-campus-numerique.fr>
2026-01-05 16:06:51 +0100

    calculs.php

--- exercices/jour-01/calculs.php
+++ exercices/jour-01/calculs.php
@@ -3,9 +3,13 @@
     $priceExcludingTax = 100;
     $vat = 0.20;  //(20 en pourcentage);
     $quantity = 3;
-   
-    echo "TVA =" .$priceExcludingTax * $vat; //"Montant de la TVA"
-    echo "Prix TTC unitaire =" .$priceExcludingTax * (1+$vat) == $prixTTC ; //Le prix TTC unitaire (Prix HT +TVA) 100  * (1+0.20)
-    echo "Total pour qté demandé =" .$prixTTC * $quantity;  //Le total pour la quantité commandée ( Prix TTC * Qté)
+    $prixTTC = $priceExcludingTax * (1+$vat);
+    echo "TVA = " .$priceExcludingTax * $vat; //"Montant de la TVA" ?>
+    <br/>
+    <?php
+    echo "Prix TTC unitaire = " .$prixTTC ; //Le prix TTC U (Prix HT +TVA) => 100  * (1+0.20) ?>
+    <br/>
+    <?php
+    echo "Total pour qté demandé = " .$prixTTC * $quantity;  //Le total pour la quantité commandée ( Prix TTC * Qté)
 
 ?>
