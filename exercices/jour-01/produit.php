b78b5e1f7bdc0608a68c04922d241dd8a645425d
Maeva Pereira <maeva.pereira@le-campus-numerique.fr>
2026-01-05 14:35:21 +0100

    produit.php

---
+++ exercices/jour-01/produit.php
@@ -0,0 +1,13 @@
+<?php
+
+    $name = "Phone";
+    var_dump($name);
+    $price = 499.99;
+    var_dump($price);
+    $stock = 12;
+    var_dump($stock);
+    $onSale = false;
+    var_dump($onSale);
+    echo "Le produit $name coûte $price";
+
+?>
