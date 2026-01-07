1efc9fcda8ed8db07f1b79fab215c1bd43ee9032
Maeva Pereira <maeva.pereira@le-campus-numerique.fr>
2026-01-07 11:41:42 +0100

    J3_ foreach-assoc.php

---
+++ exercices/jour-03/foreach-assoc.php
@@ -0,0 +1,15 @@
+<?php 
+
+    $info = [
+        "name" => "Max Caulfield",
+        "age" => "18",
+        "city" => "Arcadia Bay",
+        "job" => "Student",
+    ];
+
+
+foreach ($info as $key => $value) {
+    echo "<strong>$key</strong> : $value<br>";
+}
+
+?>
