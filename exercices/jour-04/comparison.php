56ca60ac0fc7ce7866f22defddbc5effaf7ffa5b
Maeva Pereira <maeva.pereira@le-campus-numerique.fr>
2026-01-08 11:01:12 +0100

    J4_comparison.php

--- exercices/jour-04/comparison.php
+++ exercices/jour-04/comparison.php
@@ -13,15 +13,19 @@ $e = "0";
 var_dump ($a == $b); // bool (false)
 var_dump ($a === $b); //bool (false)
 
-var_dump ($a == $c); //  bool(true) 0 null
+var_dump ($a == $c); //  bool(true) 
 var_dump ($a === $c); // bool (false)
 
 
-var_dump ($a == $d); // bool (true)  0 false
+var_dump ($a == $d); // bool (true)  
 var_dump ($a === $d); // bool (false)
 
 
 var_dump ($a == $e); // bool (true) 
 var_dump ($a === $e); // bool (false)
 
+/*
+Dans quel cas == donne un résultat inattendu ?
+$a == $c, $d, $e car le résultat de la valeur est true alors qu'avec === le résultat est faux alors plus fible d'utiliser ===
+*/
 ?>
