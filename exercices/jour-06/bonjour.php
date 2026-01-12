140d5ce6cab483bc91f3952a8a7cabed0510046e
Maeva Pereira <maeva.pereira@le-campus-numerique.fr>
2026-01-12 09:49:20 +0100

    J6_ bonjour.php

---
+++ exercices/jour-06/bonjour.php
@@ -0,0 +1,13 @@
+<?php
+
+$name = $_GET["name"];
+$age =  $_GET["age"];
+
+if($_GET["name"]. $_GET["age"]){
+   echo "Bonjour " . $_GET["name"]. "," ." vous avez ". $_GET["age"] ." ans" . " !";
+}
+else{
+    echo "Bonjour visiteur !";
+}
+
+?>
