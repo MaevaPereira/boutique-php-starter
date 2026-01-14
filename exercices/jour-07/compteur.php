9c21c024c8f259041e9a55759620412f03a76208
Maeva Pereira <maeva.pereira@le-campus-numerique.fr>
2026-01-14 12:08:45 +0100

    J7_compteur.php

---
+++ exercices/jour-07/compteur.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+//reset
+if(isset ($_GET["reset"])){
+  $_SESSION['visits']=0;
+}
+
+//compteur de visite
+if (!isset($_SESSION['visits'])) {
+  $_SESSION['visits'] = 0;
+} else {
+  $_SESSION['visits']++;
+ 
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+  <p>Vous avez visité cette page <?=$_SESSION["visits"]?> fois</p>
+  <a href= "compteur.php">Refresh</a>
+
+  <a href= "compteur.php?reset=0">Reset</a>
+
+</body>
+</html>
