068c6bb2f5981b99651053bacaba3423baafbbb8
Maeva Pereira <maeva.pereira@le-campus-numerique.fr>
2026-01-19 14:54:23 +0100

    J8_BankAccount.php

---
+++ exercices/jour-08/BankAccount.php
@@ -0,0 +1,46 @@
+<?php
+
+class BankAccount{
+
+     function __construct(private $balance){
+        $this->balance = $balance;
+    }
+    
+    // Getter : lire la valeur
+    public function getBalance(){
+        return $this->balance;
+    }
+
+    //deposit
+    public function deposit($amount){
+        $this->balance += $amount;
+    }
+
+    //withdraw => vérifier que le balance est suffisant
+    public function withdraw($amount){
+        if($amount > 0 && $amount <= $this->balance){   
+            $this->balance -= $amount;
+        }
+        echo "Aucun montant négatif autorisé";
+    }
+}
+
+$account = new BankAccount(0);
+$account-> deposit(10);
+$account-> withdraw(100);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<p>Account: <?= $account-> getBalance() ?> €</p>
+
+</body>
+</html>
